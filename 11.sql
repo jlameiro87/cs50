d04c0838e8934a903e1c02837576914a9ed73d90
jlameiro87 <31218194+jlameiro87@users.noreply.github.com>
2020-05-15 13:04:58 -0400

    Create 11.sql

---
+++ 11.sql
@@ -0,0 +1,1 @@
+SELECT movies.title FROM movies INNER JOIN ratings ON movies.id = ratings.movie_id INNER JOIN stars ON movies.id = stars.movie_id INNER JOIN people ON stars.person_id = people.id WHERE people.name = "Chadwick Boseman" ORDER BY ratings.rating DESC LIMIT 5
