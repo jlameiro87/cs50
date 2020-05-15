9b6ade30181cc0a0221e140da8a63eebfb9de968
jlameiro87 <31218194+jlameiro87@users.noreply.github.com>
2020-05-15 13:05:14 -0400

    Create 12.sql

---
+++ 12.sql
@@ -0,0 +1,1 @@
+SELECT movies.title FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE people.name = "Johnny Depp" INTERSECT SELECT movies.title FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE people.name = "Helena Bonham Carter"
